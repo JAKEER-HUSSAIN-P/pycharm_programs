08d6b164c5a9c414248893bbfa5d8e78546aab37
JAKEER-HUSSAIN-P <padegal44554548@gmail.com>
2025-07-10 15:18:43 +0530

    Delete python directory

--- python/divide.py
+++
@@ -1,8 +0,0 @@
-def divide(a,b):
-    try:
-        return a//b
-    except ZeroDivisionError:
-        return "Zero Division Error"
-a=int(input("Enter a:"))
-b=int(input("Enter b:"))
-print(divide(a,b))
--- python/operations.py
+++
@@ -1,8 +0,0 @@
-li=open("C:/Users/Lenovo/OneDrive/Desktop/example.txt","r")
-txt=li.read().split()
-txt.strip()
-cnt=1
-for i in txt:
-    if i==" ":
-        cnt+=1
-print(cnt)
